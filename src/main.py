8085330ad0be12b9bfd84ac2b81151bf60593a1d
micha <michoffmann.potsdam@gmail.com>
2017-11-04 10:55:22 +0100

    small cleanups

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ import pprint
 CONFIG_PATH = os.path.join('..', 'config.json')
 NUM_POINTS = 100
 
-
 def smoothed_curve(xs, ys, zs):
 	scale = np.linspace(0, 1, NUM_POINTS)
 	tck, _ = interpolate.splprep([xs, ys, zs], s=0.1)
@@ -27,16 +26,15 @@ if __name__ == '__main__':
     
     # add planets to plot 
     planets_x, planets_y, planets_z = [[d[pos] for d in data['planets']] for pos in ["x", "y", "z"]]
+    points_x, points_y, points_z = [[d[pos] for d in data['points']] for pos in ["x", "y", "z"]]
 
     ax.scatter(xs=planets_x, ys=planets_y, zs=planets_z, c='blue')
+    ax.scatter(xs=points_x, ys=points_y, zs=points_z, c='red')
     for d, x, y, z in zip(data['planets'], planets_x, planets_y, planets_z):
         ax.text(x, y, z, d["name"])
 
     # add curve, and knots to plot
-    points_x, points_y, points_z = [[d[pos] for d in data['points']] for pos in ["x", "y", "z"]]
     smooth_x, smooth_y, smooth_z = smoothed_curve(points_x, points_y, points_z)
-
-    ax.scatter(points_x, points_y, points_z)
     ax.plot(smooth_x, smooth_y, smooth_z)
 
     plt.show()
